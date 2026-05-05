d9ed0380f9d5ed1324b909906381e51a2ebb068d
farhanna26 <farhannaufalazmi56@gmail.com>
2026-05-05 13:10:31 +0700

    feat: berhasil setup real-time websocket pakai Laravel Reverb dan Echo

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,17 @@
-//
+// import './bootstrap';
+
+/**
+ * Echo exposes an expressive API for subscribing to channels and listening
+ * for events that are broadcast by Laravel. Echo and event broadcasting
+ * allow your team to quickly build robust real-time web applications.
+ */
+
+import './echo';
+
+// Nangkep sinyal dari Reverb
+window.Echo.channel('chat-channel')
+    .listen('.message.sent', (e) => {
+        console.log('🔥 BOOM! Pesan masuk Han:', e);
+        alert('Ada pesan baru masuk ke LearnLoop!');
+    });
+
