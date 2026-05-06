ca451c79b035b80f9977b6d10e5fcd69ae4a2b32
yazarznz-ops <yazarznz@gmail.com>
2026-05-07 02:16:01 +0700

    menyelesaikan fitur content feed: post, comment, dan like

---
+++ routes/api.php
@@ -0,0 +1,10 @@
+<?php
+
+use App\Http\Controllers\PostController;
+use Illuminate\Support\Facades\Route;
+
+// Endpoint untuk Content Feed LearnLoop
+Route::get('/posts', [PostController::class, 'index']);
+Route::post('/posts', [PostController::class, 'store']);
+Route::post('/posts/{id}/comment', [PostController::class, 'storeComment']);
+Route::post('/posts/{id}/like', [PostController::class, 'toggleLike']);
