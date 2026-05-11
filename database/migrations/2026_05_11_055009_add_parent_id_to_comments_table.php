b374106756d6eda7839c547b59eb2109790bbb06
farhanna26 <farhannaufalazmi56@gmail.com>
2026-05-11 13:27:04 +0700

    Feature: Like, Comment, and Reply System implemented

---
+++ database/migrations/2026_05_11_055009_add_parent_id_to_comments_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+{
+    Schema::table('comments', function (Blueprint $table) {
+        // parent_id buat nandain ini balesan ke komentar mana. 
+        // Nullable karena komentar utama nggak punya bapak.
+        $table->unsignedBigInteger('parent_id')->nullable()->after('post_id');
+        $table->foreign('parent_id')->references('id')->on('comments')->onDelete('cascade');
+    });
+}
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('comments', function (Blueprint $table) {
+            //
+        });
+    }
+};
